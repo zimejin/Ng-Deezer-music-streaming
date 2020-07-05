7474ef96f79b54804bcdeb03a3e0da2687da7f75
zimejin <moonspawn@live.co.uk>
2020-07-05 01:57:55 +0100

    code

--- src/app/content/partials/song-horizontal/song-horizontal.component.ts
+++ src/app/content/partials/song-horizontal/song-horizontal.component.ts
@@ -1,47 +1,73 @@
-import { Component, HostBinding, HostListener, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+} from "@angular/core";
+import { Router } from "@angular/router";
 
-import { SearchService } from '../../../core/services/search.service';
-import { AudioPlayerService } from '../../../core/services/audio-player.service';
+import { SearchService } from "../../../core/services/search.service";
+import { AudioPlayerService } from "../../../core/services/audio-player.service";
 
 @Component({
-    selector: 'app-song-horizontal',
-    templateUrl: './song-horizontal.component.html'
+  selector: "app-song-horizontal",
+  template: `
+    <div class="song-h--img">
+      <img
+        [src]="song?.cover_url"
+        alt="{{ song?.name }}"
+        [class]="imageBorderRadiusClass"
+      />
+    </div>
+    <div class="song-h--desc">
+      <span class="text-truncate song-h--name" title="{{ song?.name }}">{{
+        song?.name
+      }}</span>
+      <p
+        class="text-truncate song-h--artist"
+        title="{{ song?.artist }}"
+        *ngIf="song?.artist"
+      >
+        {{ song?.artist }}
+      </p>
+    </div>
+  `,
 })
 export class SongHorizontalComponent implements OnInit {
+  @HostBinding("class") classes = "song-h";
 
-    @HostBinding('class') classes = 'song-h';
+  @Input() song: any = {};
+  @Input() largeImage = false;
+  @Input() imageBorderRadiusClass = "card-img--radius-sm";
+  @Input() playlist: any;
+  @Input() routeLink = "";
+  @Input() songIndex: number;
 
-    @Input() song: any = {};
-    @Input() largeImage = false;
-    @Input() imageBorderRadiusClass = 'card-img--radius-sm';
-    @Input() playlist: any;
-    @Input() routeLink = '';
-    @Input() songIndex: number;
+  constructor(
+    private router: Router,
+    private searchService: SearchService,
+    private audioPlayerService: AudioPlayerService
+  ) {}
 
-    constructor(private router: Router,
-                private searchService: SearchService,
-                private audioPlayerService: AudioPlayerService) { }
-
-    @HostListener('click') onClick() {
-        this.searchService.hideSearchResult();
-        if (this.playlist) {
-            // Add playlist in audio play and play selected song
-            this.audioPlayerService.playNowPlaylist(this.playlist, this.songIndex);
-        } else if (this.routeLink) {
-            this.router.navigate([this.routeLink]);
-        } else {
-            // Play selected song
-            this.audioPlayerService.playSong(this.song);
-        }
+  @HostListener("click") onClick() {
+    this.searchService.hideSearchResult();
+    if (this.playlist) {
+      // Add playlist in audio play and play selected song
+      this.audioPlayerService.playNowPlaylist(this.playlist, this.songIndex);
+    } else if (this.routeLink) {
+      this.router.navigate([this.routeLink]);
+    } else {
+      // Play selected song
+      this.audioPlayerService.playSong(this.song);
     }
+  }
 
-    ngOnInit() {
-        if (this.largeImage) {
-            this.classes += ' song-h--lg';
-        } else {
-            this.classes += ' song-h--sm';
-        }
+  ngOnInit() {
+    if (this.largeImage) {
+      this.classes += " song-h--lg";
+    } else {
+      this.classes += " song-h--sm";
     }
-
+  }
 }
--- src/app/content/partials/song-list-view/song-list-view.component.ts
+++ src/app/content/partials/song-list-view/song-list-view.component.ts
@@ -1,26 +1,51 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from "@angular/core";
 
 @Component({
-    selector: 'app-song-list-view',
-    templateUrl: './song-list-view.component.html'
+  selector: "app-song-list-view",
+  template: `
+    <div class="song-list--item-number">
+      {{ songNumber < 10 ? "0" + songNumber : songNumber }}
+    </div>
+
+    <app-song-horizontal
+      [song]="song"
+      [imageBorderRadiusClass]="imageBorderRadiusClass"
+      [playlist]="playlist"
+      [songIndex]="songIndex"
+    ></app-song-horizontal>
+
+    <ul class="custom-card--labels d-flex ml-auto">
+      <li *ngIf="song?.favorite">
+        <span class="badge badge-pill badge-danger"
+          ><i class="la la-heart"></i
+        ></span>
+      </li>
+      <li *ngIf="song?.premium">
+        <span class="badge badge-pill badge-warning"
+          ><i class="la la-star"></i
+        ></span>
+      </li>
+      <li class="ml-4">
+        <app-song-options [icon]="icon" [song]="song"></app-song-options>
+      </li>
+    </ul>
+  `,
 })
 export class SongListViewComponent implements OnInit {
+  @HostBinding("class") classes = "song-list--item";
 
-    @HostBinding('class') classes = 'song-list--item';
+  @Input() song: any = {};
+  @Input() songNumber: number;
+  @Input() imageBorderRadiusClass = "card-img--radius-sm";
+  @Input() icon = "la-ellipsis-v";
+  @Input() playlist: any;
+  @Input() songIndex: number;
 
-    @Input() song: any = {};
-    @Input() songNumber: number;
-    @Input() imageBorderRadiusClass = 'card-img--radius-sm';
-    @Input() icon = 'la-ellipsis-v';
-    @Input() playlist: any;
-    @Input() songIndex: number;
+  constructor() {}
 
-    constructor() { }
-
-    ngOnInit() {
-        if (this.playlist) {
-            this.classes += ' amplitude-song-container amplitude-play-pause';
-        }
+  ngOnInit() {
+    if (this.playlist) {
+      this.classes += " amplitude-song-container amplitude-play-pause";
     }
-
+  }
 }
